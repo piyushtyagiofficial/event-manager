8969694b68f612867f8645ffbd62923e989df765
Piyush Kumar Tyagi <116939757+piyushtyagiofficial@users.noreply.github.com>
2024-09-06 02:16:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://event-manager-web-two.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
