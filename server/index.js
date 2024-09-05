34c0928d0fae900cd5c2ff6a1124b0a85c6e0a04
Piyush Kumar Tyagi <116939757+piyushtyagiofficial@users.noreply.github.com>
2024-09-06 01:58:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,10 +25,9 @@ mongoose.connect(process.env.MONGO_URI, {
     .catch((error) => console.error('Failed to connect to MongoDB', error));
 
 // Define your routes here
-app.get('/', (req, res) => {
-    res.send('hi');
-});
-
+app.get("/", async(req, res) => {
+  res.json("Hii");
+})
 
 app.use('/events', eventRoutes);
 
