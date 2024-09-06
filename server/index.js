26be16bd49087f7907a19286852133ff52f2837b
Piyush Kumar Tyagi <116939757+piyushtyagiofficial@users.noreply.github.com>
2024-09-06 15:37:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,9 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Define your routes here
 app.use('/events', eventRoutes);
-
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
